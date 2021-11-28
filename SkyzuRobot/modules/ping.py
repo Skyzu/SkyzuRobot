302cb922bd9c4c5d97b169cdcfba44c64b8d8b82
SkyzuProject <92871568+Skyzu@users.noreply.github.com>
2021-11-28 09:05:23 +0700

    Update ping.py

--- SkyzuRobot/modules/ping.py
+++ SkyzuRobot/modules/ping.py
@@ -79,7 +79,7 @@ def ping(update: Update, context: CallbackContext):
     uptime = get_readable_time((time.time() - StartTime))
 
     message.edit_text(
-        "<b>PONG</b> ✨\n"
+        "<b>PONG</b> \n"
         "<b>Time Taken:</b> <code>{}</code>\n"
         "<b>Service Uptime:</b> <code>{}</code>".format(telegram_ping, uptime),
         parse_mode=ParseMode.HTML,
