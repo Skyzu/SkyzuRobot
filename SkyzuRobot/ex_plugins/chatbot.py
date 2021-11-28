6db79ba2e70272936e66e55ad8d808f236cbf4bf
F I Z I • Ɱeƙípres <91390078+zeinzo@users.noreply.github.com>
2021-11-28 07:56:39 +0700

    Add files via upload

---
+++ SkyzuRobot/ex_plugins/chatbot.py
@@ -0,0 +1,33 @@
+from SkyzuRobot.mongo import client as db_x
+
+lydia = db_x["CHATBOT"]
+
+
+def add_chat(chat_id):
+    stark = lydia.find_one({"chat_id": chat_id})
+    if stark:
+        return False
+    lydia.insert_one({"chat_id": chat_id})
+    return True
+
+
+def remove_chat(chat_id):
+    stark = lydia.find_one({"chat_id": chat_id})
+    if not stark:
+        return False
+    lydia.delete_one({"chat_id": chat_id})
+    return True
+
+
+def get_all_chats():
+    r = list(lydia.find())
+    if r:
+        return r
+    return False
+
+
+def get_session(chat_id):
+    stark = lydia.find_one({"chat_id": chat_id})
+    if not stark:
+        return False
+    return stark
