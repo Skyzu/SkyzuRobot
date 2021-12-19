b032bc879d171fca885fbc729ad7765174687fae
UserLazy <78595134+UserLazy@users.noreply.github.com>
2021-12-19 18:36:23 +0700

    idk

--- SkyzuRobot/modules/sql/__init__.py
+++ SkyzuRobot/modules/sql/__init__.py
@@ -1,15 +1,25 @@
-from SkyzuRobot import DB_URI
+import sys
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+from SkyZuRobot import DB_URI, LOGGER
 
 
 def start() -> scoped_session:
     engine = create_engine(DB_URI, client_encoding="utf8")
+    LOGGER.info("PostgreSQL Connecting to database......")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
 
 BASE = declarative_base()
-SESSION = start()
+try:
+    SESSION: scoped_session = start()
+except Exception as e:
+    LOGGER.exception(f"PostgreSQL Failed to connect due to {e}")
+    sys.exit()
+
+LOGGER.info("PostgreSQL Connection successful, session started.")
