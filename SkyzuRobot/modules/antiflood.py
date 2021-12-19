93b6886fea5c127bfee667f7e0444f338719233f
UserLazy <78595134+UserLazy@users.noreply.github.com>
2021-12-19 07:32:32 +0700

    complete up ptb

--- SkyzuRobot/modules/antiflood.py
+++ SkyzuRobot/modules/antiflood.py
@@ -53,11 +53,11 @@ def check_flood(update, context) -> str:
     try:
         getmode, getvalue = sql.get_flood_setting(chat.id)
         if getmode == 1:
-            chat.kick_member(user.id)
+            chat.ban_member(user.id)
             execstrings = "Banned"
             tag = "BANNED"
         elif getmode == 2:
-            chat.kick_member(user.id)
+            chat.ban_member(user.id)
             chat.unban_member(user.id)
             execstrings = "Kicked"
             tag = "KICKED"
