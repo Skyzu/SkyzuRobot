ec75e820556dd5a6c5e31135e18fb01c8cce838d
UserLazy <78595134+UserLazy@users.noreply.github.com>
2021-12-19 07:34:24 +0700

    done

--- SkyzuRobot/modules/blacklist_stickers.py
+++ SkyzuRobot/modules/blacklist_stickers.py
@@ -435,7 +435,7 @@ def del_blackliststicker(update: Update, context: CallbackContext):
                     return
                 elif getmode == 5:
                     message.delete()
-                    chat.kick_member(user.id)
+                    chat.ban_member(user.id)
                     bot.sendMessage(
                         chat.id,
                         "{} banned because using '{}' which in blacklist stickers".format(
@@ -448,7 +448,7 @@ def del_blackliststicker(update: Update, context: CallbackContext):
                 elif getmode == 6:
                     message.delete()
                     bantime = extract_time(message, value)
-                    chat.kick_member(user.id, until_date=bantime)
+                    chat.ban_member(user.id, until_date=bantime)
                     bot.sendMessage(
                         chat.id,
                         "{} banned for {} because using '{}' which in blacklist stickers".format(
