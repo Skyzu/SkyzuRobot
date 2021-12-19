edca4375cf8e498d16a9ca337f2afcadb4eb2b33
UserLazy <78595134+UserLazy@users.noreply.github.com>
2021-12-19 18:45:35 +0700

    Update reporting_sql.py

--- SkyzuRobot/modules/sql/reporting_sql.py
+++ SkyzuRobot/modules/sql/reporting_sql.py
@@ -2,12 +2,13 @@ import threading
 from typing import Union
 
 from SkyzuRobot.modules.sql import BASE, SESSION
+from sqlalchemy.sql.sqltypes import BigInteger
 from sqlalchemy import Boolean, Column, Integer, String
 
 
 class ReportingUserSettings(BASE):
     __tablename__ = "user_report_settings"
-    user_id = Column(Integer, primary_key=True)
+    user_id = Column(BigInteger, primary_key=True)
     should_report = Column(Boolean, default=True)
 
     def __init__(self, user_id):
