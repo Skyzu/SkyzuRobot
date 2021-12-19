14a0fe55acd0253c32fa9e0322b333bd62224029
UserLazy <78595134+UserLazy@users.noreply.github.com>
2021-12-19 07:08:00 +0700

    idk about this

--- SkyzuRobot/ex_plugins/dbfunctions.py
+++ SkyzuRobot/ex_plugins/dbfunctions.py
@@ -315,15 +315,16 @@ async def is_nsfw_on(chat_id: int) -> bool:
         return True
     return False
 
+
 async def nsfw_on(chat_id: int):
-    is_nsfw = is_nsfw_on(chat_id)
+    is_nsfw = await is_nsfw_on(chat_id)
     if is_nsfw:
         return
     return nsfwdb.delete_one({"chat_id": chat_id})
 
 
 async def nsfw_off(chat_id: int):
-    is_nsfw = is_nsfw_on(chat_id)
+    is_nsfw = await is_nsfw_on(chat_id)
     if not is_nsfw:
         return
     return nsfwdb.insert_one({"chat_id": chat_id})
