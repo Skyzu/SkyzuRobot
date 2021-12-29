b085361138316d4eff8e926a288011e7dd27e43d
SkyzuProject <92871568+Skyzu@users.noreply.github.com>
2021-12-29 16:16:58 +0700

    Update tagall.py

--- SkyzuRobot/modules/tagall.py
+++ SkyzuRobot/modules/tagall.py
@@ -1,37 +1,88 @@
+import os
+import logging
 import asyncio
-from telethon import events
-from telethon.tl.types import ChannelParticipantsAdmins
-from SkyzuRobot import telethn
-from SkyzuRobot.events import register as tomori
 
+from telethon import Button, TelegramClient, events
+from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
+from telethon.tl.functions.channels import GetParticipantRequest
+from telethon.errors import UserNotParticipantError
 
-@tomori(pattern="^/tagall ?(.*)")
-async def _(event):
-    if event.fwd_from:
-        return
-    mentions = "Hi Friends I'm Skyzu I Call To All Of You"
-    chat = await event.get_input_chat()
-    async for x in telethn.iter_participants(chat, 100):
-        mentions += f" \n [{x.first_name}](tg://user?id={x.id})"
-    await event.reply(mentions)
-    await event.delete()
+from SkyzuRobot import telethn as Client
 
+spam_chats = []
 
-@tomori(pattern="^/users ?(.*)")
-async def _(event):
-    if event.fwd_from:
-        return
-    mentions = "Users : "
-    chat = await event.get_input_chat()
-    async for x in telethn.iter_participants(chat, filter=ChannelParticipantsAdmins):
-        mentions += f" \n [{x.first_name}](tg://user?id={x.id})"
-    reply_message = None
-    if event.reply_to_msg_id:
-        reply_message = await event.get_reply_message()
-        await reply_message.reply(mentions)
-    else:
-        await event.reply(mentions)
-    await event.delete()
+@Client.on(events.NewMessage(pattern="^/all ?(.*)"))
+async def mentionall(event):
+  chat_id = event.chat_id
+  if event.is_private:
+    return await event.respond("This command can be use in groups and channels!")
+  
+  is_admin = False
+  try:
+    partici_ = await Client(GetParticipantRequest(
+      event.chat_id,
+      event.sender_id
+    ))
+  except UserNotParticipantError:
+    is_admin = False
+  else:
+    if (
+      isinstance(
+        partici_.participant,
+        (
+          ChannelParticipantAdmin,
+          ChannelParticipantCreator
+        )
+      )
+    ):
+      is_admin = True
+  if not is_admin:
+    return await event.respond("Only admins can mention all!")
+  
+  if event.pattern_match.group(1) and event.is_reply:
+    return await event.respond("Give me one argument!")
+  elif event.pattern_match.group(1):
+    mode = "text_on_cmd"
+    msg = event.pattern_match.group(1)
+  elif event.is_reply:
+    mode = "text_on_reply"
+    msg = await event.get_reply_message()
+    if msg == None:
+        return await event.respond("I can't mention members for older messages! (messages which are sent before I'm added to group)")
+  else:
+    return await event.respond("Reply to a message or give me some text to mention others!")
+  
+  Spam = spam_chats.append(chat_id)
+  usrnum = 0
+  usrtxt = ''
+  async for usr in Client.iter_participants(chat_id):
+    if not chat_id in spam_chats:
+      break
+    usrnum += 1
+    usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
+    if usrnum == 10:
+      if mode == "text_on_cmd":
+        txt = f"{usrtxt}\n\n{msg}"
+        await Client.send_message(chat_id, txt)
+      elif mode == "text_on_reply":
+        await msg.reply(usrtxt)
+      await asyncio.sleep(2)
+      usrnum = 0
+      usrtxt = ''
+  try:
+    spam_chats.remove(chat_id)
+  except:
+    pass
 
+@Client.on(events.NewMessage(pattern="^/cancel$"))
+async def cancel_spam(event):
+  if not event.chat_id in spam_chats:
+    return await event.respond('There is no proccess on going...')
+  else:
+    try:
+      spam_chats.remove(event.chat_id)
+    except:
+      pass
+    return await event.respond('Stopped.')
 
-__mod_name__ = "Tagger"
+__mod_name__ = "Mentions"
