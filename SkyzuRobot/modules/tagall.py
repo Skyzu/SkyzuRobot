988ef59c2ce5d7d38a6889372f6a3d891b441584
SkyzuProject <92871568+Skyzu@users.noreply.github.com>
2021-12-29 16:48:50 +0700

    Update tagall.py

--- SkyzuRobot/modules/tagall.py
+++ SkyzuRobot/modules/tagall.py
@@ -6,7 +6,7 @@ from telethon.tl.types import ChannelParticipantAdmin
 from telethon.tl.types import ChannelParticipantCreator
 from telethon.tl.functions.channels import GetParticipantRequest
 from telethon.errors import UserNotParticipantError
-from EmikoRobot import telethn as client
+from SkyzuRobot import telethn as client
 
 
 spam_chats = []
