f384b53dcfecbf57d9a67d1239e263eeca892474
SkyzuProject <92871568+Skyzu@users.noreply.github.com>
2022-04-23 21:16:27 +0700

    Update __main__.py

--- SkyzuRobot/__main__.py
+++ SkyzuRobot/__main__.py
@@ -84,7 +84,7 @@ PM_START_TEXT = """
 *Hello {} !*
 ────────────────────────
 ✪ I'm an anime theme bot designed to help manage your telegram group with a lot features.
-✪ Maintained by @skyzu ✨
+✪ Maintained by @xflskyzu ✨
 ────────────────────────
 Hit the /help to see available command.
 """
